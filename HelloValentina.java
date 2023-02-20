f4de4d51669071a3c5bb902684a0e4cea47f2f71
Il tuo nome <tu@esempio.com>
2023-02-20 10:33:37 +0100

    20/02

---
+++ HelloValentina.java
@@ -0,0 +1,9 @@
+// per aprire un file java aprire un terminal nuovo con "javac + nome file" invio
+//per richiamare il file "java + nome file"
+
+public class HelloValentina {
+    public static void main(String[]args){
+System.out.println("Hello World!");
+    }
+}
+
